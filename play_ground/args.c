85071b4a1fe2fed1b75760a052cefd1e77142563
Masaki Miyauchi <masaki@miyauchi.me>
2019-05-02 15:50:06 +0900

    Add playground dir.

---
+++ play_ground/args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+  int i;
+
+  printf("argc=%d\n", argc);
+  for(i = 0; i < argc; i++) {
+    printf("argv[%d]=%s\n", i, argv[i]);
+  }
+  
+  return 0;
+}
